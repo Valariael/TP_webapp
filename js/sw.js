7a5f1ca201c70ca02e3436f6e30b4d0c6a75f466
Axel Ledermann <aledermann4@gmail.com>
2019-11-20 14:43:02 +0100

    sw completed

--- js/sw.js
+++ js/sw.js
@@ -1,7 +1,3 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
 var cacheName = 'TP_webapp';
 var appShellFiles = [
   '/index.html',
@@ -31,3 +27,46 @@ var appShellFiles = [
   '/icons/icon-256.png',
   '/icons/icon-512.png'
 ];
+
+/*var images = [];
+for(var i=0; i<games.length; i++) {
+  images.push('data/img/'+games[i].slug+'.jpg');
+}
+var contentToCache = appShellFiles.concat(images);*/
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+        if(cacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
